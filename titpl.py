c9c4a4d0ecd542a57896551db1559392e2a61b1c
0xTheSmartGuy <101930484+0xTheSmartGuy@users.noreply.github.com>
2022-10-23 16:12:26 +0200

    Make faster using very Pythonic code
    Trying to actually build up more Pythonic code

--- titpl.py
+++ titpl.py
@@ -26,6 +26,7 @@ def evaluatefile(file):
    for i in m:
       loop += 1
       nonparsed=str(i).strip('[]')
+      distilled=nonparsed.replace(";", "")
       try:
           x=str(i).strip('[]').split(" ")
       except:
@@ -34,31 +35,31 @@ def evaluatefile(file):
        if nonparsed[len(nonparsed)-1] == ";": # We will be sure there is a comma at THE RIGHT PLACE
         if x == "define":
          markervar = True
-        elif markervar == True:
-           varlst.append(x.replace(";", "")) # Generally little sneak thing: The name of the var is in fact ;x but we finsed it with replace!
+        elif markervar:
+           varlst.append(distilled) # Generally little sneak thing: The name of the var is in fact ;x but we fixed it with replace! (this solution is deprecated since distilled is used!)
            markervar=0
-        elif x in varlst:
+        elif distilled in varlst:
             markervar="mz"
-            cvar=x
+            cvar=distilled
         elif markervar == "mz":
-            if x == "=":
+            if distilled == "=":
              markervar="stayalert"
         elif markervar == "stayalert":
-            if x.isdigit():
-             intvar[cvar] == x
+            if distilled.isdigit():
+             intvar[cvar] == distilled
             else:
                  print(bcolors.FAIL+bcolors.BOLD+"tit: line "+ str(loop)+ ": assignement not int!")
                  sys.exit()
-        elif x == "end":
+        elif distilled == "end":
             sys.exit()
-        elif x == "printf":
+        elif distilled == "printf":
             markervar="PrintF"
         elif markervar == "PrintF":
-            print(intvar[x])
-        elif "@" in x:
+            print(intvar[distilled])
+        elif "@" in distilled:
             pass
        else:
-            print(bcolors.WARNING+bcolors.BOLD+"tit: line "+str(loop)+": comma incorrectly placed!")
+            print(bcolors.WARNING+bcolors.BOLD+"tit: warning : line "+str(loop)+": comma incorrectly placed!")
             print(bcolors.WARNING+"passing line "+str(loop)+"\x00\x00"+bcolors.BOLD+": tit")
       else:
            print(bcolors.FAIL+bcolors.BOLD+"tit: line "+str(loop)+": no comma!")   
